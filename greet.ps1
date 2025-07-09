48263d6527305caffb0c10fee633a5289480f741
R0keBee <letcherliam@proton.me>
2025-07-08 23:46:18 -0700

    Update greet.ps1

--- greet.ps1
+++ greet.ps1
@@ -1,2 +1,9 @@
 Add-Type -AssemblyName System.Windows.Forms
-[System.Windows.Forms.MessageBox]::Show("Hello from PowerShell!", "Greeting")
+[System.Windows.Forms.MessageBox]::Show("idiot!", "Greeting")
+
+regkey='HKCU:\Control Panel\Desktop\NewKey'
+$regparam='testparameter'
+if (Get-ItemProperty -Path $regkey -Name $regparam -ErrorAction Ignore)
+{ write-host 'The registry entry already exist' }
+else
+{ New-ItemProperty -Path $regkey -Name $regparam -Value ”woshub_test”  -PropertyType "String"  }
