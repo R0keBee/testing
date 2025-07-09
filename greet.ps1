8545c135ef55d334b52af23d4d0f74724d75253a
R0keBee <letcherliam@proton.me>
2025-07-09 05:37:11 -0700

    Update greet.ps1

--- greet.ps1
+++ greet.ps1
@@ -1,11 +1,11 @@
 Add-Type -AssemblyName System.Windows.Forms
 [System.Windows.Forms.MessageBox]::Show("idiot!", "Greeting")
 # Set the path to the script or executable
-$regName = "Personal use"
-$scriptPath = "$env:USERPROFILE\Desktop\greet.ps1"
+#$regName = "Personal use"
+#$scriptPath = "$env:USERPROFILE\Desktop\greet.ps1"
 
-New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" `
-                 -Name $regName `
-                 -Value "powershell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File `"$scriptPath`"" `
-                 -PropertyType String -Force
+#New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" `
+                 #-Name $regName `
+                 #-Value "powershell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File `"$scriptPath`"" `
+                # -PropertyType String -Force
 
